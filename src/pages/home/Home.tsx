b73de8f15b6a456f06f1c9517ec5a8e6cdf23b76
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-19 12:39:07 +0000

    Added email newsletter area

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,6 +5,7 @@ import Featured from '../../components/featured/Featured'
 import './home.css'
 import PropertyList from "../../components/propertyList/PropertyList"
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
+import MailList from '../../components/mailList/MailList'
 const Home = () => {
   return (
     <div>
@@ -17,6 +18,7 @@ const Home = () => {
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties/>
       </div>
+      <MailList/>
     </div>
   )
 }
