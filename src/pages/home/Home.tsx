cc1e0f75e7a377d59d97eb27d9275b441b1c277f
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-19 08:52:20 +0000

    Added a placeholder image to featured page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Header from '../../components/header/Header'
+import Featured from '../../components/featured/Featured'
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Header type={undefined}/>
       <div className="homeContainer">
+        <Featured/>
         
       </div>
     </div>
