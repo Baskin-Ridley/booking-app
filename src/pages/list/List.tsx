eb5a6792d5eed5f0e41296f529e86c0d114008c8
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-22 13:49:19 +0000

    Updated the list component to have more details

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+//@ts-nocheck
+import {useState} from 'react'
 import Navbar from '../../components/navbar/navbar'
 import Header from '../../components/header/Header'
 import "./list.css"
 import { useLocation } from 'react-router-dom'
+import { format } from 'date-fns'
+import { DateRange } from 'react-date-range'
 const List = () => {
 
   const location = useLocation()
   console.log(location)
+  const [destination,setDestination] = useState(location.state.destination)
+  const [date, setDate] = useState(location.state.date)
+  const [options, setOptions] = useState(location.state.options)
 
   return (
     <div>
@@ -18,11 +24,17 @@ const List = () => {
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
               <label htmlFor="destination">Destination</label>
-              <input type="text" />
+              <input placeholder={destination} type="text" />
             </div>
             <div className="lsItem">
               <label htmlFor="check-in date">Check-in Date</label>
-              <input type="text" />
+              <span>
+                {`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}
+                </span>
+                <DateRange
+                onChange={item => setDate([item.selection])}
+                minDate={new Date()}
+                />
             </div>
             <div className="lsItem">
               <label htmlFor="Destination">Destination</label>
