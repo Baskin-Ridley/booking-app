63e47a79b443acc15ece95855fd442d5b604db4f
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-23 11:01:54 +0000

    Building out hotel page

--- src/pages/hotel/Hotel.tsx
+++ src/pages/hotel/Hotel.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import "./hotel.css"
+import Navbar from "../../components/navbar/navbar"
+import Header from "../../components/header/Header"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
 
 const Hotel = () => {
   return (
-    <div>Hotel</div>
+    <>
+      <Navbar/>
+      <Header type="list"/>
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">King's Cross Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot}/>
+            <span>Kings Cross Road, London</span>
+          </div>
+          <span className="hotelDistance">Excellent Location, city centre </span>
+          <span className="hotelPriceHighLight">Book a stay over $120 at this property and get a free taxi</span>
+          <div className="hotelImages">
+            {/* note these images will eventually be fetched from the backend*/}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
